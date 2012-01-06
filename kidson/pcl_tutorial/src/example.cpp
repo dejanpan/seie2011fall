9c0b8e49c4a2b8f12f1d97c01bb3b9841ce3530c
rossk <rossk@a37698f0-4fc0-4799-a845-b7378c75482a>
2012-01-06 17:15:34 +0000

    rename topic

--- kidson/pcl_tutorial/src/example.cpp
+++ kidson/pcl_tutorial/src/example.cpp
@@ -27,7 +27,7 @@ int
 main (int argc, char** argv)
 {
   // Initialize ROS
-  ros::init (argc, argv, "my_pcl_tutorial");
+  ros::init (argc, argv, "pcl_tutorial");
   ros::NodeHandle nh;
 
   // Create a ROS subscriber for the input point cloud
