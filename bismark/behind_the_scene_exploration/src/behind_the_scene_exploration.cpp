d69dd367c43d5ca565e3c208be8bfe4202ab2955
dinamex <dinamex@a37698f0-4fc0-4799-a845-b7378c75482a>
2012-01-12 23:48:04 +0000

    transform of pointcloud / not working

--- bismark/behind_the_scene_exploration/src/behind_the_scene_exploration.cpp
+++ bismark/behind_the_scene_exploration/src/behind_the_scene_exploration.cpp
@@ -1,32 +1,44 @@
-#include <ros/ros.h>
-#include <simple_robot_control/robot_control.h>
 #include <math.h>
+#include "ros/ros.h"
+#include "simple_robot_control/robot_control.h"
+#include "pcl/point_types.h"
+#include "pcl/io/pcd_io.h"
+#include "pcl_ros/transforms.h"
+#include "pcl/filters/voxel_grid.h"
+#include "tf/transform_listener.h"
 
 using namespace std;
 
+
+
 int main(int argc, char** argv){
 
-	ros::init(argc, argv, "robot_control_test_app");
-    ros::NodeHandle nh;
+
+	ros::init(argc, argv, "behind_the_scene_exploration");
+	ros::NodeHandle n;
 
     //Create robot controller interface
     simple_robot_control::Robot robot;
 
 
-
-
     float r,x,y,z,yaw_angle, pitch_angle,roll ;
-    //radius of the circle trajectory
-    r=0.2;
 
-    //z offset of the trajectory (wrt. base_link)
-    z=1.0;
+    r=0.2;			//radius of the circle trajectory
+    z=1.0;			//z offset of the trajectory (wrt. base_link)
     roll = M_PI;
 
     tf::Transform trans;
     tf::Quaternion axis;
+    tf::TransformListener tf_;
+
+    sensor_msgs::PointCloud2 input_pc, transformed_pc;
+
+    sensor_msgs::PointCloud2::Ptr output_pc (new sensor_msgs::PointCloud2 ());
+	sensor_msgs::PointCloud2::Ptr output_pc_filtered (new sensor_msgs::PointCloud2 ());
 
 //+++++++++++++inital pose++++++++++++++++++++++++++++++++
+
+
 
     robot.torso.move(0.4);
 
@@ -39,9 +51,43 @@ int main(int argc, char** argv){
 
 
 
+
 //+++++++++++++exploring right side of the scene++++++++++++++++++++++++++++++++
 
     for(int i=220;i<=320;i+=20){
+
+//++++++++++++++++++Pointcloud processing++++++++++++++++++++++++++++++++++++++++
+
+
+    	bool found_transform = tf_.waitForTransform("eye_in_hand_rgb_optical_frame", "base_link",
+													ros::Time::now(), ros::Duration(10.0));
+
+
+    	  if (found_transform)
+    	  {
+    	    tf::StampedTransform transform;
+    	    tf_.lookupTransform("base_link","eye_in_hand_rgb_optical_frame", ros::Time::now(), transform);
+    	    pcl_ros::transformPointCloud("base_link", transform, input_pc, transformed_pc);
+    	  }
+
+    	  output_pc += transformed_pc;
+
+
+    	  // Create the filtering object
+    	  pcl::VoxelGrid<sensor_msgs::PointCloud2> sor;
+    	  sor.setInputCloud (output_pc);
+    	  sor.setLeafSize (0.01f, 0.01f, 0.01f);
+    	  sor.filter (*output_pc_filtered);
+
+
+    	  pcl::PCDWriter writer;
+    	  writer.write ("eye_in_hand_scene_downsampled.pcd", *output_pc_filtered,
+    	         Eigen::Vector4f::Zero (), Eigen::Quaternionf::Identity (), false);
+
+
+
+//++++++++++++++++++END processing Pointcloud++++++++++++++++++++++++++++++++++++++++
+
 
             //x value for circle trajectory
             x= ((cos((M_PI*i)/180))*r)+0.52;
@@ -65,49 +111,6 @@ int main(int argc, char** argv){
 
 
     }
-
-//+++++++++++++top view of the scene++++++++++++++++++++++++++++++++
-
-//        yaw_angle = 0;
-//        pitch_angle = 0.5*M_PI;
-//        axis.setRPY(0.0,pitch_angle,yaw_angle);
-//
-//
-//        tf::StampedTransform topview_pose (tf::Transform(axis,
-//        	tf::Vector3(0.9,-0.1,0.9)), ros::Time::now(),"base_link","doesnt_matter");
-//        robot.right_arm.moveGrippertoPose(topview_pose, 20);
-
-
-
-//+++++++++++++exploring left side of the scene++++++++++++++++++++++++++++++++
-
-//    for(int i=60;i<=140;i+=20){
-//
-//            //x value for circle trajectory
-//            x= ((cos((M_PI*i)/180))*r)+0.5;
-//            //y value for circle trajectory
-//            y= ((sin((M_PI*i)/180))*r)-0.25;
-
-            //angles in radians (gripper looks to the center of the circle)
-//            yaw_angle = 1.5*M_PI;
-//            pitch_angle = 0.1*M_PI;
-//
-//            //converting angles into quaternions
-//            axis.setRPY(roll,pitch_angle,yaw_angle);
-//
-//            //transformation to the new pose (wrt. base_link)
-//            tf::StampedTransform poses_on_leftside (tf::Transform(axis,tf::Vector3(x,y,z)),
-//            ros::Time::now(),"base_link","doesnt_matter");
-//            robot.right_arm.moveGrippertoPose(poses_on_leftside, 20);
-//
-//            cout<<"degree = "<<i << endl;
-//            cout<<"pitch = "<<pitch_angle <<"  yaw = "<<yaw_angle<< endl;
-//
-//
-//    }
-
-
-
 
 
 
