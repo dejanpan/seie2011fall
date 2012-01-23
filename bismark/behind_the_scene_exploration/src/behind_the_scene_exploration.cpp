bcc746efb91416086118be940053e41f13658719
dinamex <dinamex@a37698f0-4fc0-4799-a845-b7378c75482a>
2012-01-23 22:29:25 +0000

    transform of pointcloud / not tested

--- bismark/behind_the_scene_exploration/src/behind_the_scene_exploration.cpp
+++ bismark/behind_the_scene_exploration/src/behind_the_scene_exploration.cpp
@@ -6,6 +6,7 @@
 #include "pcl_ros/transforms.h"
 #include "pcl/filters/voxel_grid.h"
 #include "tf/transform_listener.h"
+#include "pcl/registration/icp.h"
 
 using namespace std;
 
@@ -25,19 +26,24 @@ int main(int argc, char** argv){
     r=0.2;			//radius of the circle trajectory
     z=1.0;			//z offset of the trajectory (wrt. base_link)
     roll = M_PI;
+    bool first_concat = true;
 
     tf::Transform trans;
     tf::Quaternion axis;
     tf::TransformListener tf_;
 
-    sensor_msgs::PointCloud2::Ptr input_pc (new sensor_msgs::PointCloud2 ());
-    sensor_msgs::PointCloud2::Ptr transformed_pc (new sensor_msgs::PointCloud2 ());
 
+    sensor_msgs::PointCloud2::ConstPtr input_pc;
+    sensor_msgs::PointCloud2::Ptr output_pc_filtered (new sensor_msgs::PointCloud2 ());
+    //sensor_msgs::PointCloud2::Ptr concat_pc (new sensor_msgs::PointCloud2 ());
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr pcl_concat_pc (new pcl::PointCloud<pcl::PointXYZRGB>);
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr pcl_transformed_pc (new pcl::PointCloud<pcl::PointXYZRGB>);
 //+++++++++++++inital pose++++++++++++++++++++++++++++++++
 
 
 
     robot.torso.move(0.4);
+    robot.left_arm.tuck();
 
     double init_pos_right[]
            = { -1.74, -0.35, -2.36, -2.12, 7.13, -1.00, 3.52,
@@ -46,78 +52,94 @@ int main(int argc, char** argv){
         robot.right_arm.goToJointPos(init_pos_vec);
 
 
-
-
-
 //+++++++++++++exploring right side of the scene++++++++++++++++++++++++++++++++
 
     for(int i=220;i<=320;i+=20){
 
 //++++++++++++++++++Pointcloud processing++++++++++++++++++++++++++++++++++++++++
 
+    	//get input pointcloud from rosbag
+    	input_pc = ros::topic::waitForMessage<sensor_msgs::PointCloud2>("eye_in_hand/depth_registered/points");
+    	pcl::PointCloud<pcl::PointXYZRGB>::Ptr pcl_input_pc (new pcl::PointCloud<pcl::PointXYZRGB>);
+    	pcl::fromROSMsg(*input_pc, *pcl_input_pc);
 
 
-
-		  //transformation of Pointcloud
-		  bool found_transform = tf_.waitForTransform("eye_in_hand_rgb_optical_frame", "base_link",
-													ros::Time::now(), ros::Duration(10.0));
-
-    	  if (found_transform)
-    	  {
-    	    tf::StampedTransform transform;
-    	    tf_.lookupTransform("base_link","eye_in_hand_rgb_optical_frame", ros::Time::now(), transform);
-    	    pcl_ros::transformPointCloud("base_link", transform, *input_pc, *transformed_pc);
-    	  }
+    	//transformation of Pointcloud
+		//bool found_transform = tf_.waitForTransform("eye_in_hand_rgb_optical_frame", "base_link", ros::Time::now(), ros::Duration(10.0));
+    	bool found_transform = tf_.waitForTransform( "base_link", "eye_in_hand_rgb_optical_frame", ros::Time::now()-ros::Duration(5), ros::Duration(10.0));
 
 
-    	  //Convert PC2 to PointXYZ and concatenate the Pointclouds
-    	  pcl::PointCloud<pcl::PointXYZ>::Ptr pcl_transformed_pc (new pcl::PointCloud<pcl::PointXYZ>);
-    	  pcl::PointCloud<pcl::PointXYZ>::Ptr pcl_concat_pc (new pcl::PointCloud<pcl::PointXYZ>);
-    	  pcl::fromROSMsg(*transformed_pc, *pcl_transformed_pc);
-    	  *pcl_concat_pc += *pcl_transformed_pc;
-
-    	  //Filtering of Pointcloud
-    	  sensor_msgs::PointCloud2::Ptr output_pc_filtered (new sensor_msgs::PointCloud2 ());
-    	  pcl::VoxelGrid<sensor_msgs::PointCloud2> sor;
-    	  sor.setInputCloud (pcl_concat_pc);
-    	  sor.setLeafSize (0.01f, 0.01f, 0.01f);
-    	  sor.filter (*output_pc_filtered);
+    	if (found_transform)
+    	{
 
 
+    		tf::StampedTransform transform;
+    		//tf_.lookupTransform("eye_in_hand_rgb_optical_frame","base_link", ros::Time::now()-ros::Duration(5), transform);
+    		pcl_ros::transformPointCloud( "base_link", *pcl_input_pc, *pcl_transformed_pc, tf_);
+    		cerr<<"TRANSFORMATION SUCCESS"<<endl;
+    	}
+
+    	else
+    		continue;
+
+    	if(first_concat){
+    		pcl_concat_pc->header = pcl_transformed_pc->header;
+    		*pcl_concat_pc = *pcl_transformed_pc;
+    	}
+
+    	else
+    		*pcl_concat_pc += *pcl_transformed_pc;						//concatenation of clouds
+
+    	cerr<<"concat frame id: "<<pcl_concat_pc->header.frame_id<<endl;
+    	cerr<<"transformed frame id: "<<pcl_transformed_pc->header.frame_id<<endl;
+    	first_concat=false;
 
 
+//    	//Filtering of Pointcloud
+//    	pcl::VoxelGrid<sensor_msgs::PointCloud2> sor;
+//    	sor.setInputCloud (pcl_concat_pc);
+//    	sor.setLeafSize (0.01f, 0.01f, 0.01);
+//    	sor.filter (*output_pc_filtered);
+
+//    	pcl::IterativeClosestPoint<pcl::PointXYZRGB, pcl::PointXYZRGB> icp;
+//    	icp.setInputCloud(*pcl_concat_pc);  // boost shared pointer ??!?
+//    	icp.setInputTarget(*pcl_transformed_pc); // boost shared pointer ??!?
+//    	pcl::PointCloud<pcl::PointXYZRGB> Final;
+//    	icp.align(Final);
 
 //++++++++++++++++++END processing Pointcloud++++++++++++++++++++++++++++++++++++++++
 
 
-            //x value for circle trajectory
-            x= ((cos((M_PI*i)/180))*r)+0.52;
-            //y value for circle trajectory
-            y= ((sin((M_PI*i)/180))*r)-0.2;
+    	//x value for circle trajectory
+        x= ((cos((M_PI*i)/180))*r)+0.52;
+        //y value for circle trajectory
+        y= ((sin((M_PI*i)/180))*r)-0.2;
 
-            //angles in radians (gripper looks to the center of the circle)
-            yaw_angle = ((float)i/640)*M_PI;
-            pitch_angle = 0.1*M_PI;
+        //angles in radians (gripper looks to the center of the circle)
+        yaw_angle = ((float)i/600)*M_PI;
+        pitch_angle = 0.1*M_PI;
 
-            //converting angles into quaternions
-            axis.setRPY(roll,pitch_angle,yaw_angle);
+        //converting angles into quaternions
+        axis.setRPY(roll,pitch_angle,yaw_angle);
 
-            //transformation to the new pose (wrt. base_link)
-            tf::StampedTransform poses_on_rightside (tf::Transform(axis,tf::Vector3(x,y,z)),
-            ros::Time::now(),"base_link","doesnt_matter");
-            robot.right_arm.moveGrippertoPose(poses_on_rightside, 10);
+        //transformation to the new pose (wrt. base_link)
+        tf::StampedTransform poses_on_rightside (tf::Transform(axis,tf::Vector3(x,y,z)),
+        ros::Time::now(),"base_link","doesnt_matter");
+        robot.right_arm.moveGrippertoPose(poses_on_rightside, 10);
 
-            cout<<"degree = "<<i << endl;
-            cout<<"pitch = "<<pitch_angle <<"  yaw = "<<yaw_angle<< endl;
+        cout<<"degree = "<<i << endl;
+        cout<<"pitch = "<<pitch_angle <<"  yaw = "<<yaw_angle<< endl;
 
 
     }
 
-	  //Save observed scene to pcd file
-	  pcl::PCDWriter writer;
-	  writer.write ("eye_in_hand_scene_downsampled.pcd", *output_pc_filtered,
-	         Eigen::Vector4f::Zero (), Eigen::Quaternionf::Identity (), false);
+    //Save observed scene to pcd file
+    pcl::PCDWriter writer;
+//    writer.write ("eye_in_hand_scene_downsampled.pcd", *output_pc_filtered,
+//        Eigen::Vector4f::Zero (), Eigen::Quaternionf::Identity (), false);
 
+    //Save observed scene without filtering
+    writer.write ("eye_in_hand_scene_downsampled.pcd", *pcl_concat_pc);
 
         return 0;
 
