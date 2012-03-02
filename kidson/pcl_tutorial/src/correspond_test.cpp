66018748aab0ff6dd082f58db46f50dcdaf235b2
rossk <rossk@a37698f0-4fc0-4799-a845-b7378c75482a>
2012-03-02 17:51:17 +0000

    files for pcl compilation

---
+++ kidson/pcl_tutorial/src/correspond_test.cpp
@@ -0,0 +1,9 @@
+#include <pcl/registration/correspondence_estimation.h>
+
+int
+ main (int argc, char** argv)
+{
+  pcl::registration::CorrespondenceEstimation<pcl::PointXYZ, pcl::PointXYZ> corr_est;
+
+ return (0);
+}
