cb456cfd7db39038edad6e49b02fe82189b524f5
rimzahir <rimzahir@a37698f0-4fc0-4799-a845-b7378c75482a>
2012-03-19 01:16:51 +0000

    binaries

--- zahir/androidObjectRecognition_Tracking/build/CMakeFiles/download_data_data_box.png.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/download_data_data_box.png"
-  "../data/box.png"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/download_data_data_box.png.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
--- zahir/androidObjectRecognition_Tracking/build/CMakeFiles/download_data_data_box_in_scene.png.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/download_data_data_box_in_scene.png"
-  "../data/box_in_scene.png"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/download_data_data_box_in_scene.png.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
