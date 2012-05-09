b95f7244f2239943f8ce6ae218997abd7e873a04
vsu91 <vsu91@a37698f0-4fc0-4799-a845-b7378c75482a>
2012-05-09 10:08:29 +0000

    added function for voting and saving matrix

--- usenko/furniture_classification/src/classify.cpp
+++ usenko/furniture_classification/src/classify.cpp
@@ -15,8 +15,34 @@
 #include <pcl/common/transforms.h>
 #include <pcl/visualization/pcl_visualizer.h>
 #include <pcl/console/parse.h>
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
+#include <fstream>
+
+void voteToGrid(
+		pcl::PointCloud<pcl::PointXYZI> & model_centers,
+		Eigen::MatrixXf & grid, const pcl::PointXYZ & min_bound,
+		const pcl::PointXYZ & max_bound, float cell_size) {
+
+	int image_x_width = (int) ((max_bound.x - min_bound.x) / cell_size);
+	int image_y_width = (int) ((max_bound.y - min_bound.y) / cell_size);
+
+	grid = Eigen::MatrixXf::Zero(image_x_width, image_y_width);
+
+	for (size_t i = 0; i < model_centers.points.size(); i++) {
+		int vote_x = (model_centers.points[i].x - min_bound.x) / cell_size;
+		int vote_y = (model_centers.points[i].y - min_bound.y) / cell_size;
+		if ((vote_x >= 0) && (vote_y >= 0) && (vote_x < image_x_width)
+				&& (vote_y < image_y_width))
+			grid(vote_x, vote_y) += model_centers.points[i].intensity;
+	}
+}
+
+void saveGridToPGMFile(const std::string & filename, const Eigen::MatrixXf & grid) {
+	Eigen::MatrixXi img = (grid * 255.0 / grid.maxCoeff()).cast<int> ();
+
+	std::ofstream f(filename.c_str());
+	f << "P2\n" << grid.cols() << " " << grid.rows() << "\n255\n";
+	f << img;
+}
 
 int main(int argc, char** argv) {
 
@@ -30,13 +56,14 @@ int main(int argc, char** argv) {
 
 	std::string database_file_name = "database.yaml";
 	std::string scene_file_name;
-	//"data/scans/Sideboards/Spatio_Sideboard_160_0000F1C5_squished-centered/rotation0_distance4_tilt-15_shift0.pcd";
 	int min_points_in_segment = 300;
 	int num_neighbours = 1;
-	float cell_size = 0.01;
+	float cell_size = 0.1;
 
-	pcl::console::parse_argument(argc, argv, "-database_file_name", database_file_name);
-	pcl::console::parse_argument(argc, argv, "-scene_file_name", scene_file_name);
+	pcl::console::parse_argument(argc, argv, "-database_file_name",
+			database_file_name);
+	pcl::console::parse_argument(argc, argv, "-scene_file_name",
+			scene_file_name);
 	pcl::console::parse_argument(argc, argv, "-num_neighbours", num_neighbours);
 
 	databaseType database;
@@ -120,9 +147,6 @@ int main(int argc, char** argv) {
 	pcl::io::loadPCDFile(scene_file_name, scene);
 	pcl::PointXYZ min_bound, max_bound;
 	pcl::getMinMax3D<pcl::PointXYZ>(scene, min_bound, max_bound);
-	int image_x_width = (int) ((max_bound.x - min_bound.x) / cell_size);
-	int image_y_width = (int) ((max_bound.y - min_bound.y) / cell_size);
-	std::cout << "Size " << image_x_width << "x" << image_y_width << std::endl;
 
 	for (std::map<std::string, pcl::PointCloud<pcl::PointXYZI> >::const_iterator
 			it = votes.begin(); it != votes.end(); it++) {
@@ -131,28 +155,10 @@ int main(int argc, char** argv) {
 
 		pcl::io::savePCDFileASCII(class_name + ".pcd", model_centers);
 
-		cv::Mat image = cv::Mat::zeros(image_x_width, image_y_width, CV_32FC1);
+		Eigen::MatrixXf grid;
+		voteToGrid(model_centers, grid, min_bound, max_bound, cell_size);
 
-		for (size_t i = 0; i < model_centers.points.size(); i++) {
-			int vote_x = (model_centers.points[i].x - min_bound.x) / cell_size;
-			int vote_y = (model_centers.points[i].y - min_bound.y) / cell_size;
-			if ((vote_x > 0) && (vote_y > 0) && (vote_x < image.rows)
-					&& (vote_x < image.cols) && (model_centers.points[i].z > 0))
-				image.at<float> (vote_x, vote_y)
-						+= model_centers.points[i].intensity;
-		}
-
-		cv::Mat image_normalized;
-		cv::normalize(image, image_normalized, 0.0, 255.0, cv::NORM_MINMAX);
-
-		cv::circle(image_normalized, cv::Point(-min_bound.x / cell_size,
-				-min_bound.y / cell_size), 4, 255);
-
-		//cv::Mat image_normalized_char;
-		//image_normalized.convertTo(image_normalized_char, CV_8UC3);
-		//image_normalized_char.at<char> (-min_bound.x / cell_size, -min_bound.y
-		//		/ cell_size, 1) = 250;
-		cv::imwrite(class_name + ".png", image_normalized);
+		saveGridToPGMFile(class_name + ".pgm", grid);
 
 	}
 
